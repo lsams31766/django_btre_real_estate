2b9b855ffdbdb02d5a0d35bc897f427ab712b034
Larry Samuels <larry_samuels@hotmail.com>
2020-08-12 12:01:54 -0400

    update index and search

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
+from .choices import bedroom_choices, price_choices, state_choices
 
 def index(request):
     # sort by list date descending
@@ -21,4 +22,9 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html',context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices
+    }
+    return render(request, 'listings/search.html', context)
