1fc2acd36b7aae7e59f4f89b168438d752711207
Larry Samuels <larry_samuels@hotmail.com>
2020-08-13 11:30:18 -0400

    register validation and authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,42 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
         # Register logic here
-        return
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password2 = request.POST['password2']
+        password = request.POST['password']
+        # check passwords match
+        if password == password2:
+            # check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                # check eamil
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is taken')
+                    return redirect('register')
+                else:
+                    # Looks good
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    # Option 1 - login after register
+                    # auth.login(request,user)
+                    # messages.success(request, 'You are now logged in')
+                    # return redirect('index')
+                    # Option 2 - register but ask to login
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
+
+        else:
+            messages.error(request,'Passwords do not match')
+            return redirect('register')
     else:
         return render(request,'accounts/register.html')
 
--- btre/settings.py
+++ btre/settings.py
@@ -135,6 +135,11 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
 
 #superuser name: larry
 #superuser password: lsams07041 
