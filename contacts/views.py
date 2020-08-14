7a08d4f692e40d445aace7c5d47533ab2c4ca53b
Larry Samuels <larry_samuels@hotmail.com>
2020-08-14 09:00:48 -0400

    finish dashboard

--- contacts/views.py
+++ contacts/views.py
@@ -13,6 +13,16 @@ def contact(request):
         message = request.POST['message']
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
+
+        #check if user already submitted this
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                print("ALREADY LISETED")
+                messages.error(request, 'You have already made an inquiry for this listing.')
+                return redirect('/listings/'+listing_id)
+
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id )
         contact.save()
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
